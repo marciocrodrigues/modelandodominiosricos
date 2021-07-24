730b2650b4328ae196329a806ae5f7aa1df8e0a8
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2021-07-24 01:10:59 -0300

    commit inicial

---
+++ NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Pega todas as entidades mapeadas, verifica se é string e transforma para
+            // varchar(100) para não nvarchar ou varchar(max)
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            // Verifica no ChangeTracker "Mapeador de mudanças" se existe a propriedade cadastro
+            // Verifica se está adicionando Added
+            // Verifica se está Modificando, vai ignorar o campo datacadastro para não alterar
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
